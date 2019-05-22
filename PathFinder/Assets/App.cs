a48b542d0a31f9d3293a61ddc62926452dd9afe1
lexnewgate <lexnewgate@gmail.com>
2019-05-23 02:28:37 +0800

    create grid

--- PathFinder/Assets/App.cs
+++ PathFinder/Assets/App.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class App : MonoBehaviour
@@ -10,22 +11,24 @@ public class App : MonoBehaviour
     public GameObject cell;
     private GameObject[,] blocks;
 
-    void Start()
+    async void Start()
     {
         blocks=new GameObject[CellsAcross,CellsUp];
-        for (int i = 0; i < CellsUp; i++)
+
+        for (int j = 0; j < CellsAcross; j++)
         {
-            for (int j = 0; j < CellsAcross; j++)
+            for (int i = 0; i < CellsUp; i++)
             {
-                var block= Object.Instantiate<GameObject>(cell);
-                block.transform.position=new Vector3(i,0,j);
-                block.transform.SetParent(ground.transform,true);
+                var block = Object.Instantiate<GameObject>(cell);
+                block.transform.position = new Vector3(i, 0, j);
+                block.transform.SetParent(ground.transform, true);
                 block.name = $"{j} {i}";
                 blocks[j, i] = block;
+                await new WaitForSeconds(0.2f);
             }
         }
 
-   }
+    }
 
     void OnDrawGizmos()
     {
@@ -38,7 +41,10 @@ public class App : MonoBehaviour
             for (int j = 0; j < CellsAcross; j++)
             {
                 Gizmos.color = Color.green;
-                Gizmos.DrawSphere(blocks[j,i].transform.position,0.2f);
+                if (blocks[j, i] != null)
+                {
+                    Gizmos.DrawSphere(blocks[j,i].transform.position,0.2f);
+                }
             }
         }
     }
